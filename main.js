e6d5f97785c99962adb9902c979e50a4d6382b72
ojou-less <mark.fischer0404@gmail.com>
2023-06-07 23:56:18 +0200

    aktueller stand + neue heatmap library

--- main.js
+++ main.js
@@ -20,36 +20,6 @@ slider.oninput = function() {
   ySliderValue = this.value;
 }
 
-/*  start legend code */
-// we want to display the gradient, so we have to draw it
-// var legendCanvas = document.createElement('canvas');
-// legendCanvas.width = 100;
-// legendCanvas.height = 10;
-var legendCanvas = document.getElementById("legendCanvas");
-var min = document.querySelector('#min');
-var max = document.querySelector('#max');
-var gradientImg = document.querySelector('#gradient');
-var legendCtx = legendCanvas.getContext('2d');
-var gradientCfg = {};
-function updateLegend(data) {
-  // the onExtremaChange callback gives us min, max, and the gradientConfig
-  // so we can update the legend
-  min.innerHTML = data.min;
-  max.innerHTML = data.max;
-  // regenerate gradient image
-  if (data.gradient != gradientCfg) {
-    gradientCfg = data.gradient;
-    var gradient = legendCtx.createLinearGradient(0, 0, 100, 1);
-    for (var key in gradientCfg) {
-      gradient.addColorStop(key, gradientCfg[key]);
-    }
-    legendCtx.fillStyle = gradient;
-    legendCtx.fillRect(0, 0, 100, 10);
-    gradientImg.src = legendCanvas.toDataURL();
-  }
-};
-/* end legend code */
-
 // minimal heatmap instance configuration
 var heatmapInstance = h337.create({
     // only container is required, the rest will be defaults
@@ -91,8 +61,8 @@ var heatmapInstance = h337.create({
   // now generate some random data
   var points = [];
   var max = 0;
-  var width = 840;
-  var height = 400;
+  var width = 300;
+  var height = 840;
   var len = 200;
   
   while (len--) {
